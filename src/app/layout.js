1cbf5994a939d91d680905630878b0665e9ed46e
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-08-12 22:21:15 +0600

    about page added

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,13 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Next Hero',
   description: 'Hero Description',
-  position:"CEO"
+
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}{pisition}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
