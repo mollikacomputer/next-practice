e1c64bb31f6c7d5f8625695dbf2315b4b1274904
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-08-13 22:28:01 +0600

    nested route added

--- src/app/dashboard/users/page.js
+++ src/app/dashboard/users/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const adminPage = () => {
     return (
         <div>
-            <h2>Admin page</h2>
+            <h2>Admin page Nested Route admin page</h2>
         </div>
     );
 };
