73915e50ee2e51489025dd9e0cab849d49f397cb
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-08-15 00:08:56 +0600

    dynamic year implement

--- src/app/blogs/[[...segment]]/page.js
+++ src/app/blogs/[[...segment]]/page.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const page = ({params}) => {
-    const [year, id] = params.segment;
+    const [year, id] = params.segment || [];
     return (
         <div>
-            <h2> Dynamic page {year} for {id} </h2>
+            <h2> Dynamic page {year || new Date().getFullYear()} for {id} </h2>
         </div>
     );
 };
