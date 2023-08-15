74ca1ae6247073200297c16eeb5c2084da50b51e
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-08-15 23:08:33 +0600

    not found page implement

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const notFound = () => {
+    return (
+        <div>
+            <h2>404</h2>
+            <h2> Not Found page</h2>
+        </div>
+    );
+};
+
+export default notFound;
