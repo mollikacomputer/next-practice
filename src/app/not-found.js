24192c61ef2b706c5680fbd54885b9be20bae3a9
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-08-18 23:47:59 +0600

    services page added

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,6 +5,8 @@ const notFound = () => {
         <div>
             <h2>404</h2>
             <h2> Not Found page</h2>
+            <p> Page not found at this moment</p>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum eum quod ut veritatis commodi facilis? Ratione, rem eius. Sapiente, quam.</p>
         </div>
     );
 };
---
+++ src/app/services/services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const services = () => {
+    return (
+        <div>
+            Our services
+        </div>
+    );
+};
+
+export default services;
